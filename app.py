904523c85ff702e261a28b97023c2adb3aa4f364
Ally Feng <fengall@cs.washington.edu>
2022-04-12 16:03:07 -0700

    updating webcam_emotion_detection.py to take in image

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from flask import Flask
 from flask import render_template, request
+import numpy as np
+from PIL import Image
+import base64
+import re
+from io import BytesIO
+
 
 app = Flask(__name__)
 
@@ -8,8 +14,23 @@ def index():
     return render_template("index.html")
 
 @app.route("/analyze", methods=["POST"])
-def analayze():
+def analyze():
+    data_url = request.values['imageBase64']
+    # Decoding base64 string to bytes object
+    img_bytes = base64.b64decode(re.sub('^data:image/.+;base64,', '', data_url))
+    img = Image.open(BytesIO(img_bytes))
+    img  = np.array(img)
+    return {
+        "data": img.tolist()
+    }
+
+    """image_b64 = request.values['imageBase64']
+    image_b64 = re.sub('^data:image/.+;base64,', '', image_b64).decode('base64')
+    image_PIL = Image.open(io.StringIO(image_b64))
+    image_np = np.array(image_PIL)
+    print('Image received: {}'.format(image_np.shape))
+    return image_np
     return {
         "emotion": "happy",
         "confidence": 1.0
-    }
+    }"""
