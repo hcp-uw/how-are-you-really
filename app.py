74d63285f098c1387bba4fac8cb7f9c4ff4c9ff3
Sangwon Kim <kittyjls13@gmail.com>
2022-10-08 00:07:33 -0700

    added live video with emotion detection

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
-from flask import Flask
-from flask import render_template, request
-import numpy as np
-from PIL import Image
-import base64
-import re
-from io import BytesIO
+from flask import Flask, Response
+from flask import render_template
 import webcam_emotion_detection
 
 app = Flask(__name__)
@@ -13,16 +8,6 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/analyze", methods=["POST"])
-def analyze():
-    data_url = request.values["imageBase64"]
-    # decode base64 string to bytes object
-    img_bytes = base64.b64decode(re.sub("^data:image/.+;base64,", "", data_url))
-    img = Image.open(BytesIO(img_bytes))
-    img = np.array(img)
-    data = webcam_emotion_detection.readFace(img)
-    return data
-
 @app.route("/video_feed")
 def video_feed():
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(webcam_emotion_detection.gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
