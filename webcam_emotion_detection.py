128540850b71b9b3032b59540bcbb7f337b5136f
Sangwon Kim <kittyjls13@gmail.com>
2022-10-19 03:26:14 -0700

    addressed some TODOs

--- webcam_emotion_detection.py
+++ webcam_emotion_detection.py
@@ -1,6 +1,6 @@
 import numpy as np
 from numpy import asarray
-import math, time, random
+import time
 
 # load saved model
 from tensorflow.keras.models import model_from_json
@@ -12,24 +12,11 @@ import cv2
 face_haar_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 # "database" storing timestamp, emotion, and confidence
-# TODO: should be initially empty
-database = [
-    {"timestamp": "0", "emotion": "happy", "confidence": 0.105},
-    {"timestamp": "10", "emotion": "neutral", "confidence": 0.802},
-    {"timestamp": "20", "emotion": "happy", "confidence": 0.12},
-    {"timestamp": "30", "emotion": "sad", "confidence": 0.5},
-    {"timestamp": "40", "emotion": "sad", "confidence": 0.405},
-    {"timestamp": "50", "emotion": "happy", "confidence": 0.701},
-    {"timestamp": "60", "emotion": "happy", "confidence": 0.99}
-]
+database = []
 
 # predict emotion in detected face in stream webcam video
 camera = cv2.VideoCapture(0)
 def gen_frames():
-    frame_rate = camera.get(cv2.CAP_PROP_FPS)
-    frame_per_second = 1 # 1 fps; should be <= frame_rate
-
-    current_frame = 0
     while True:
         success, frame = camera.read()
         if not success:
@@ -58,30 +45,26 @@ def gen_frames():
                     emotion_prediction = emotion_detection[max_index]
                     confidence = str(np.max(predictions[0]))
                     # save data to "database" every second
-                    # TODO: doesn't work; remove logic for 1 fps;
-                    # instead, record every frame and add api for computing average
-                    if current_frame % (math.floor(frame_rate / frame_per_second)) == 0:
-                        database.append({
-                            "timestamp": time.time(),
-                            "emotion": emotion_prediction,
-                            "confidence": confidence
-                        })
-                        print("timestamp: {}, emotion: {}, confidence: {}".format(
-                            time.time(),
-                            emotion_prediction,
-                            confidence)
-                        )
-                current_frame += 1
+                    database.append({
+                        "timestamp": time.time(),
+                        "emotion": emotion_prediction,
+                        "confidence": confidence
+                    })
+                    print("timestamp: {}, emotion: {}, confidence: {}".format(
+                        time.time(),
+                        emotion_prediction,
+                        confidence
+                    ))
             except:
                 pass
 
             # return frames
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
+            ret, buf = cv2.imencode('.jpg', frame)
+            frame = buf.tobytes()
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 # return the last entry in "database"
 # TODO: configure number of data points and interval to return
 def data():
-    return database[-1]
+    return database[-1:]
