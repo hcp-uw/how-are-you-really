69eec1bb941ee0c2361ed23136dc523c5f10972f
Batina Shikhalieva <batina.shikhalieva@sambanovasystems.com>
2022-10-20 10:10:03 -0700

    fixes to mental state logic

--- webcam_emotion_detection.py
+++ webcam_emotion_detection.py
@@ -13,23 +13,25 @@ face_haar_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 sessions = {}
 session_id = 0
+<<<<<<< Updated upstream
 analysis_per_session = []
 # "database" storing timestamp, emotion, and confidence
 database = []
 
+=======
+analysis_per_session = {}
+>>>>>>> Stashed changes
 # predict emotion in detected face in stream webcam video
 current_frame = 0
-i = 0
 
 camera = cv2.VideoCapture(0)
 def gen_frames():
     global current_frame
     global session_id
-    global i
     global sessions
+    global analysis_per_session
     while True:
         current_frame += 1
-        print("HELLO #", current_frame)
         # NOTE: YES! It's called multi[ple times here but while always executes once
         success, frame = camera.read()
         if not success:
@@ -58,7 +60,6 @@ def gen_frames():
                     emotion_prediction = emotion_detection[max_index]
                     confidence = str(np.max(predictions[0]))
                     print("The patient is {} with confidence {}".format(emotion_prediction, confidence))
-                    print(current_frame % 10 != 0)
 
                     # logic for mental state:
                     # 0. for single session: collect emotions in database for every 1 min, 
@@ -78,28 +79,28 @@ def gen_frames():
                     # for current session, record emotions under sessions[session_id]
                     # FIXME: this whole file is called once per one frame
                     if current_frame % 10 != 0:
-                        print("I'm IF, ", session_id)
+
                         if session_id not in sessions:
                             sessions[session_id] = []
+
                         sessions[session_id].append ({
                             "timestamp": time.time(),
                             "emotion": emotion_prediction,
                             "confidence": confidence
                         })
-                        print(sessions)
                     else:
-                        print("I'm ELSE!")
+                        sessions[session_id].append ({
+                            "timestamp": time.time(),
+                            "emotion": emotion_prediction,
+                            "confidence": confidence
+                        })
                         # analyze current session, record under new "database", potentially will fix later
                         # keep track of which percent of patients emotions falls under which quadrant of Russel's Emotional Model
                         # conceptualize these quadrants in different ways: as the dimensions of positive and negative affect, tension and energy, approach and withdrawal, or valence and arousal.
-                        for frame in sessions[session_id]:
-                            print("I'm FOR LOOP")
+                        for frame_sample in sessions[session_id]:
                             #print("session_id len(sessions[session_id])", session_id, len(sessions[session_id]))
-                            if len(analysis_per_session[session_id]) == 0:
-                                analysis_per_session[session_id].HV = 0
-                                analysis_per_session[session_id].LV = 0
-                                analysis_per_session[session_id].HA = 0
-                                analysis_per_session[session_id].HL = 0
+                            if session_id not in analysis_per_session:
+                                analysis_per_session[session_id] = {"HV": 0, "LV": 0, "HA": 0, "LA": 0}
                             # 'angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral'
                             # The Positive and Negative Affect Schedule (PANAS) 
                             # NA = negative affect, PA = positive affect
@@ -117,33 +118,45 @@ def gen_frames():
                             # high arousal + HV (high valence) = alertness, enthusiasm
                             # low arousal + HV (high valence) = enjoyment, calm positive state
                             # 'happy' = HA + HV, 'sad' = LA + LV, 'neutral' = LA + HV, 'angry' = HA + LV, 'disgust' = LV + HA 
-                            if frame.emotion == 'sad':
-                                analysis_per_session[session_id].LA += 1
-                                analysis_per_session[session_id].LV += 1
-                            elif frame.emotion == 'happy':
-                                analysis_per_session[session_id].HA += 1
-                                analysis_per_session[session_id].HV += 1
-                            elif frame.emotion == 'neutral':
-                                analysis_per_session[session_id].LA += 1
-                                analysis_per_session[session_id].HV += 1
-                        
-                        analysis_per_session[session_id].LA = round(analysis_per_session[session_id].LA / 60, 3)
-                        analysis_per_session[session_id].HA = round(analysis_per_session[session_id].HA / 60, 3)
+                            if frame_sample["emotion"] == 'sad':
+                                analysis_per_session[session_id]["LA"] += 1
+                                analysis_per_session[session_id]["LV"] += 1
+                            elif frame_sample["emotion"] == 'happy':
+                                analysis_per_session[session_id]["HA"] += 1
+                                analysis_per_session[session_id]["HV"] += 1
+                            elif frame_sample["emotion"] == 'neutral':
+                                analysis_per_session[session_id]["LA"] += 1
+                                analysis_per_session[session_id]["HV"] += 1
+                            elif frame_sample["emotion"] == 'fear':
+                                analysis_per_session[session_id]["LA"] += 1
+                                analysis_per_session[session_id]["LV"] += 1
+                            elif frame_sample["emotion"] == 'angry':
+                                analysis_per_session[session_id]["HA"] += 1
+                                analysis_per_session[session_id]["LV"] += 1
+                            elif frame_sample["emotion"] == 'disgust':
+                                analysis_per_session[session_id]["HA"] += 1
+                                analysis_per_session[session_id]["LV"] += 1    
 
-                        analysis_per_session[session_id].LV = round(analysis_per_session[session_id].LV / 60, 3)
-                        analysis_per_session[session_id].HV = round(analysis_per_session[session_id].HV / 60, 3)
+                        # NOTE: it won't sump up to 1 as 2 meotions correlate, SO 2 OF THEM SUM UP TO 1
+                        # TODO: count # frames in session, then do / (#frames) 
+                        analysis_per_session[session_id]["LA"] = round(analysis_per_session[session_id]["LA"] / 10, 3)
+                        analysis_per_session[session_id]["HA"] = round(analysis_per_session[session_id]["HA"] / 10, 3)
+
+                        analysis_per_session[session_id]["LV"] = round(analysis_per_session[session_id]["LV"] / 10, 3)
+                        analysis_per_session[session_id]["HV"] = round(analysis_per_session[session_id]["HV"] / 10, 3)
 
                         print("#########################################################")
-                        print("{} of patient's emotional state falls under LA {}".format(analysis_per_session[session_id].LA))
-                        print("{} of patient's emotional state falls under HA {}".format(analysis_per_session[session_id].HA))
-                        print("{} of patient's emotional state falls under LV {}".format(analysis_per_session[session_id].LV))
-                        print("{} of patient's emotional state falls under HV {}".format(analysis_per_session[session_id].HV))
+                        print("{} of patient's emotional state falls under LA".format(analysis_per_session[session_id]["LA"]))
+                        print("{} of patient's emotional state falls under HA".format(analysis_per_session[session_id]["HA"]))
+                        print("{} of patient's emotional state falls under LV".format(analysis_per_session[session_id]["LV"]))
+                        print("{} of patient's emotional state falls under HV".format(analysis_per_session[session_id]["HV"]))
                         print("#########################################################")
 
                         # skip first session since there's nothing we can compare it against
                         # if len(sessions) > 1:
                         # TODO: compare with prev average
                         session_id +=1
+<<<<<<< Updated upstream
                 
                     # save data to "database" every second
                     database.append({
@@ -156,12 +169,20 @@ def gen_frames():
                         emotion_prediction,
                         confidence
                     ))
+=======
+>>>>>>> Stashed changes
             except:
                 pass
 
             # return frames
+<<<<<<< Updated upstream
             ret, buf = cv2.imencode('.jpg', frame)
             frame = buf.tobytes()
+=======
+            
+            ret, buffer = cv2.imencode('.jpg', frame) #FIXME why it fails?
+            frame = buffer.tobytes()
+>>>>>>> Stashed changes
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
