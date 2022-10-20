ab1e437587dd04edd36ad23ab0f02c8f102a0d05
Batina Shikhalieva <batina.shikhalieva@sambanovasystems.com>
2022-10-20 11:34:13 -0700

    removed comments

--- webcam_emotion_detection.py
+++ webcam_emotion_detection.py
@@ -11,10 +11,10 @@ model.load_weights('model/model.h5')
 import cv2
 face_haar_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
+# mental state analysis "databases"
 sessions = {}
 session_id = 0
 analysis_per_session = {}
-# predict emotion in detected face in stream webcam video
 current_frame = 0
 
 # "database" storing timestamp, emotion, and confidence
@@ -28,7 +28,6 @@ def gen_frames():
     global analysis_per_session
     while True:
         current_frame += 1
-        # NOTE: YES! It's called multi[ple times here but while always executes once
         success, frame = camera.read()
         if not success:
             break
@@ -63,23 +62,6 @@ def gen_frames():
                     })
                     print("The patient is {} with confidence {}".format(emotion_prediction, confidence))
 
-                    # logic for mental state:
-                    # 0. for single session: collect emotions in database for every 1 min, 
-                    # 1.analyze session, We can keep track of which percent of patients emotions falls under which quadrant. 
-                    # Then we can conceptualize these quadrants in different ways: as the dimensions of positive and negative affect, tension and energy, approach and withdrawal, or valence and arousal.
-                    # 2. store in another database
-                    # 3. show progression over sessions, alert if unusual
-
-                    # ########1. potentially option to start a new session to compare over sessions?
-                    # TODO: for now, assuming one session = 60 sec (or 60 analyzed frames)
-                    #       later, 1. create a button inside the portal to "Start a new session"
-                    #              2. count how many times button was pressed = session_id
-                    #              3. each time button is pressed, update session_id in the backend
-                    #              4. in UI, have button to stop session and output the analysis to the portal
-                    
-                    # every minute, start next session
-                    # for current session, record emotions under sessions[session_id]
-                    # FIXME: this whole file is called once per one frame
                     if current_frame % 10 != 0:
 
                         if session_id not in sessions:
@@ -96,25 +78,10 @@ def gen_frames():
                             "emotion": emotion_prediction,
                             "confidence": confidence
                         })
-                        # analyze current session, record under new "database", potentially will fix later
-                        # keep track of which percent of patients emotions falls under which quadrant of Russel's Emotional Model
-                        # conceptualize these quadrants in different ways: as the dimensions of positive and negative affect, tension and energy, approach and withdrawal, or valence and arousal.
                         for frame_sample in sessions[session_id]:
-                            #print("session_id len(sessions[session_id])", session_id, len(sessions[session_id]))
                             if session_id not in analysis_per_session:
                                 analysis_per_session[session_id] = {"HV": 0, "LV": 0, "HA": 0, "LA": 0}
-                            # 'angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral'
-                            # The Positive and Negative Affect Schedule (PANAS) 
-                            # NA = negative affect, PA = positive affect
-                            # high NA is characterized by distress, anger, contempt, and nervousness or fear
-                            #            tend to worry excessively about errors and threats, and they may also be sensitive to even minimal stressors
-                            # high PA is characterized by joy and high levels of energy, concentration, enthusiasm, and alertness
-                            # PA/NA traits reflect the tendency to react negatively or positively to life events
-                            # NA is positively correlated with stress related to goal attainment and inter-goal conflicts and 
-                            # PA is positively correlated with enjoyment
-                            # source: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7063053/
-                            # Increased levels of negative affect and decreased positive affect are associated with the depressive state or mood
-                            
+
                             # high arousall (i.e. intensity) + LV (low valence i.e. balance of positivity/neagtivity) = anxiety
                             # low arousal + LV (low valence) = depression
                             # high arousal + HV (high valence) = alertness, enthusiasm
@@ -139,8 +106,6 @@ def gen_frames():
                                 analysis_per_session[session_id]["HA"] += 1
                                 analysis_per_session[session_id]["LV"] += 1    
 
-                        # NOTE: it won't sump up to 1 as 2 meotions correlate, SO 2 OF THEM SUM UP TO 1
-                        # TODO: count # frames in session, then do / (#frames) 
                         analysis_per_session[session_id]["LA"] = round(analysis_per_session[session_id]["LA"] / 10, 3)
                         analysis_per_session[session_id]["HA"] = round(analysis_per_session[session_id]["HA"] / 10, 3)
 
@@ -154,9 +119,6 @@ def gen_frames():
                         print("{} of patient's emotional state falls under HV".format(analysis_per_session[session_id]["HV"]))
                         print("#########################################################")
 
-                        # skip first session since there's nothing we can compare it against
-                        # if len(sessions) > 1:
-                        # TODO: compare with prev average
                         session_id +=1
             except:
                 pass
